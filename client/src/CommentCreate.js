c3c79de2b5a8632765d4cdcf8c9918fad7ad7e56
Helen Gu <helengu1996@gmail.com>
2020-11-05 23:15:08 -0500

    bug fix: changed react-scripts version to enable docker

--- client/src/CommentCreate.js
+++ client/src/CommentCreate.js
@@ -4,7 +4,7 @@ export default function CommentCreate({ postId }) {
 	const [content, setContent] = useState('');
 	const onSubmit = async (e) => {
 		e.preventDefault();
-		await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+		await axios.post(`http://posts.com/posts/${postId}/comments`, {
 			content,
 		});
 		setContent('');
